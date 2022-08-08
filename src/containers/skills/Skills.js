205f90c9b721b5bc09869d06f03fc0d556d8166e
amaan laskar <laskaramaan@gmail.com>
2022-08-08 21:09:18 +0530

    ui change

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -13,7 +13,7 @@ export default function Skills() {
     return null;
   }
   return (
-    <div className={isDark ? "dark-mode main" : "main"} id="skills">
+    <div className={ "dark-mode main" } id="skills">
       <div className="skills-main-div">
         <Fade left duration={1000}>
           <div className="skills-image-div">
@@ -27,15 +27,14 @@ export default function Skills() {
         <Fade right duration={1000}>
           <div className="skills-text-div">
             <h1
-              className={isDark ? "dark-mode skills-heading" : "skills-heading"}
+              className={ "dark-mode skills-heading" }
             >
               {skillsSection.title}{" "}
             </h1>
             <p
               className={
-                isDark
-                  ? "dark-mode subTitle skills-text-subtitle"
-                  : "subTitle skills-text-subtitle"
+                 "dark-mode subTitle skills-text-subtitle"
+                  
               }
             >
               {skillsSection.subTitle}
@@ -47,9 +46,8 @@ export default function Skills() {
                   <p
                     key={i}
                     className={
-                      isDark
-                        ? "dark-mode subTitle skills-text"
-                        : "subTitle skills-text"
+                       "dark-mode subTitle skills-text"
+                        
                     }
                   >
                     {skills}
